9830da78831d4f7061bf03ff6f1e5bc300d2533e
prabhanshupawar-tech <prabhanshupawar@gmail.com>
2026-08-15 18:09:05 +0530

    Task 3

---
+++ Cognifyz-Task-3/server.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+const PORT=3000;
+
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);   
+});
